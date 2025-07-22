ecb01a4c488ae436e0c73029f59a21d96ba6deac
blazey66 <blazeyhere@gmail.com>
2025-07-22 09:51:42 +0530

    Mi439: Drop MiuiCam
    Signed-off-by: blazey66 <blazeyhere@gmail.com>

--- device.mk
+++ device.mk
@@ -119,13 +119,3 @@ PRODUCT_PACKAGES += \
     GrapheneOSCamera \
     GrapheneFrameworks
 endif
-
-# EXTRA: MiuiCamera
-ifneq ($(wildcard vendor/miuicamera/config.mk),)
-#$(call inherit-product, vendor/miuicamera/config.mk)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/miuicam/pine.xml:$(TARGET_COPY_OUT_VENDOR)/etc/device_features/pine.xml \
-    $(LOCAL_PATH)/configs/miuicam/olive.xml:$(TARGET_COPY_OUT_VENDOR)/etc/device_features/olive.xml \
-    $(LOCAL_PATH)/configs/miuicam/olive.xml:$(TARGET_COPY_OUT_VENDOR)/etc/device_features/olivelite.xml \
-    $(LOCAL_PATH)/configs/miuicam/olive.xml:$(TARGET_COPY_OUT_VENDOR)/etc/device_features/olivewood.xml
-endif
